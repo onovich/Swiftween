e18ee588667c94159f05bc79ac03e96e3bb9b144
onovich <hjn1110@163.com>
2025-04-01 00:46:42 +0800

    <feature> fix: 插值时不应用 start 作为 current 传入

--- Sources/Swiftween/Swiftween.swift
+++ Sources/Swiftween/Swiftween.swift
@@ -2,8 +2,9 @@ import SwiftUI
 import Combine
 
 class Swiftween<T: Easable>: ObservableObject {
-    @Published var animatedValue: T
-    private var targetValue: T
+    @Published var currentValue: T
+    private let startValue: T
+    private var endValue: T
     private var duration: TimeInterval
     private var easeType: EasingType
     private var easeMode: EasingMode
@@ -11,8 +12,9 @@ class Swiftween<T: Easable>: ObservableObject {
     private var progress: Float = 0
 
     init(from: T, to target: T, duration: TimeInterval, easeType: EasingType, easeMode: EasingMode) {
-        self.animatedValue = from
-        self.targetValue = target
+        self.startValue = from
+        self.currentValue = from
+        self.endValue = target
         self.duration = duration
         self.easeType = easeType
         self.easeMode = easeMode
@@ -36,6 +38,7 @@ class Swiftween<T: Easable>: ObservableObject {
         progress += Float(1.0 / (60.0 * duration))
         progress = min(progress, 1.0)
 
-        animatedValue = T.ease(from: animatedValue, to: targetValue, progress: progress, easeType: easeType, easeMode: easeMode)
+        // 始终以初始值为起点
+        currentValue = T.ease(from: startValue, to: endValue, progress: progress, easeType: easeType, easeMode: easeMode)
     }
 }
